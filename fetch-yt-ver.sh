140d8eb305d2a3e3b72853351fa1ac45c1fc4bed
AzimsTech <azimstech@gmail.com>
2026-03-26 03:00:30 +0800

    fix: correct whitespace handling in fetch-yt-ver.sh

--- fetch-yt-ver.sh
+++ fetch-yt-ver.sh
@@ -5,7 +5,7 @@ latest=$(java -jar morphe-cli-*-all.jar list-patches \
   --with-packages --with-versions --patches patches-*.mpp \
   --filter-package-name=com.google.android.youtube \
   | grep -E '^\s+[0-9]+\.[0-9]+\.[0-9]+\s*$' \
-  | tr -d ' ' \
+  | tr -d ' \t' \
   | sort -uV \
   | tail -n1)
 
