bb0cc6701f862d3f1ffcb69a77d377490fc6cc74
AzimsTech <azimstech@gmail.com>
2026-04-28 12:31:26 +0800

    update build-revanced.yml and fetch-yt-ver.sh

--- fetch-yt-ver.sh
+++ fetch-yt-ver.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 latest=$(java -jar revanced-cli-*-all.jar list-patches \
-  --packages --versions -bp patches-*-dev.*.rvp \
+  --packages --versions -bp patches-*.rvp \
   --filter-package-name=com.google.android.youtube \
   | grep -E '^\s+[0-9]+\.[0-9]+\.[0-9]+\s*$' \
   | tr -d ' \t' \
